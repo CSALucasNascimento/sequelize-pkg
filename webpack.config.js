7841a40f628f3dd5542c256fc065ec55ff6f349c
Lucas Nascimento <csalucasnascimento@gmail.com>
2020-10-28 21:37:30 +1100

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/models/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  externals: {
+    sequelize: 'require("sequelize")'
+  },
+  resolve: {
+    fallback: {
+      fs: false,
+      path: false
+    }
+  }
+};
