2794cefe2d96b5f8879ec2f00390ccecffed2b2c
Lucas Nascimento <csalucasnascimento@gmail.com>
2020-10-29 01:15:29 +1100

    test seq

--- src/models/index.js
+++ src/models/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
 const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
+// const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
@@ -15,15 +15,21 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
+// fs
+//   .readdirSync(__dirname)
+//   .filter(file => {
+//     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+//   })
+//   .forEach(file => {
+//     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+//     db[model.name] = model;
+//   });
+
+const context = require.context('.', true, /^\.\/(?!index\.js).*\.js$/, 'sync')
+context.keys().map(context).forEach(module => {
+  const sequelizeModel = module(sequelize, Sequelize);
+  db[sequelizeModel.name] = sequelizeModel;
+})
 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,19 @@ module.exports = {
   externals: {
     sequelize: 'require("sequelize")'
   },
-  target: 'node'
+  target: "node",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          }
+        }
+      }
+    ]
+  }
 };
